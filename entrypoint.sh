04db97acf41fbb925a4518f7554a3edc56fe6695
thomas10-10 <36126914+thomas10-10@users.noreply.github.com>
2021-07-21 00:26:58 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@ wget https://dl.minio.io/server/minio/release/linux-amd64/minio
 chmod +x minio
 export MINIO_ACCESS_KEY="$ACCESS_KEY"
 export MINIO_SECRET_KEY="$SECRET_KEY"
-./minio server --address :$PORT /mnt/minio/data
+./minio server  --console-address $:PORT /mnt/minio/data
